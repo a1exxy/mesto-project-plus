c8f8b254d2b05d747dfc5e18f0bb77e7abf0e94f
alex <alex@localhost>
2023-11-21 02:29:17 +0300

    v6

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,12 +1,9 @@
 import mongoose from 'mongoose';
 import validator from 'validator';
-import bcrypt from 'bcrypt';
-import CustomError from '../utils/customError';
 import {
   defaultAbout,
   defaultAvatar,
   defaultName,
-  saltSize,
 } from '../config';
 
 interface IUser {
@@ -17,12 +14,7 @@ interface IUser {
   password: string
 }
 
-interface UserModel extends mongoose.Model<IUser> {
-  // eslint-disable-next-line no-unused-vars
-  createIfNotExist: (options: IUser) => Promise<mongoose.Document<unknown, any, IUser>>
-}
-
-const userSchema = new mongoose.Schema<IUser, UserModel>({
+const userSchema = new mongoose.Schema<IUser>({
   name: {
     type: String,
     minlength: 2,
@@ -52,27 +44,5 @@ const userSchema = new mongoose.Schema<IUser, UserModel>({
     select: false,
   },
 }, { toObject: { useProjection: true }, toJSON: { useProjection: true } });
-
-userSchema.static('createIfNotExist', async function createIfNotExist(options: IUser) {
-  return this.findOne({ email: options.email })
-    .then((user) => {
-      if (!user) {
-        return bcrypt.hash(options.password, saltSize)
-          .then((hash) => this.create(
-            {
-              name: options.name,
-              about: options.about,
-              avatar: options.avatar,
-              email: options.email,
-              password: hash,
-            },
-          )
-            // eslint-disable-next-line no-underscore-dangle
-            .then((out) => Promise.resolve(out)))
-          .catch(() => Promise.reject(new CustomError(500, 'На сервере произошла ошибка')));
-      }
-      return Promise.reject(new CustomError(409, 'Почта уже существует'));
-    });
-});
 
 export default mongoose.model<IUser>('user', userSchema);
